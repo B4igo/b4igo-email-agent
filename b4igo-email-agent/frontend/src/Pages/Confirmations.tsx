d048d05abbe09abf180ce38fa2e0d7081e82e271
William Herbert <Vastspacewill@gmail.com>
2026-02-07 10:15:14 -0600

    fixed a small type error real quick

--- b4igo-email-agent/frontend/src/Pages/Confirmations.tsx
+++ b4igo-email-agent/frontend/src/Pages/Confirmations.tsx
@@ -2,10 +2,11 @@
 import {useState} from "react";
 import {confirmations} from "../API/Confirmations.ts";
 import {useRepeatingFetch} from "../Functions/Helpers.ts";
+import type {Confirmation} from "../Domain/Confirmation.ts";
 
 export const Confirmations = () => {
 
-    const [Confirmations, SetConfirmations] = useState([]);
+    const [Confirmations, SetConfirmations] = useState<Confirmation[]>([]);
 
     useRepeatingFetch(confirmations.getAll, SetConfirmations, 10000)
 
