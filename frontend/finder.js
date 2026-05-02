04af7a67879680c5a4ec59ae6416c142cc3beaba
William Herbert <Vastspacewill@gmail.com>
2026-05-02 12:37:02 -0500

    created the scripts to scrape the allPrivateKeys for the extension to use for authentication

---
+++ frontend/finder.js
@@ -0,0 +1,30 @@
+﻿console.log("B4iGo Agentic Email Parser: Login script injected.");
+
+function checkTokenAndSend() {
+    const keys = localStorage.getItem("allPrivateKeys");
+
+    if (keys) {
+        console.log("B4iGo Agentic Email Parser: Found");
+
+        chrome.runtime.sendMessage({
+            type: "B4IGO_KEYS_FOUND",
+            token: keys
+        });
+
+        return true;
+    }
+    return false;
+}
+
+let found = checkTokenAndSend();
+
+if (!found) {
+    let attempts = 0;
+
+    const tokenInterval = setInterval(() => {
+        attempts++;
+        if (checkTokenAndSend()) {
+            clearInterval(tokenInterval);
+        }
+    }, 1000 + attempts * 100);
+}
